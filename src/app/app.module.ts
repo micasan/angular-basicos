c39a634457ec3755bd5c786cea4049b7d7809842
Mica <micaela.sansat@gmail.com>
2021-03-12 18:02:49 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppComponent } from './app.component';
+import { ContadorModule } from './contador/contador.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HeroesModule,
+    ContadorModule
   ],
+
   providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
